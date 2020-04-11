bb0127089877b747e941b119faaf50390dd25842
Andrew Reid <andrew.ce.reid@gmail.com>
2020-04-11 11:17:47 -0400

    Added the ability to animate logistic fits also.

--- log_progression.py
+++ log_progression.py
@@ -10,12 +10,13 @@ from scipy.optimize import curve_fit
 from scipy.stats import describe, t
 import os
 
-def model(x,a,b):
-    return a*(1+b)**x
-
-def jacobian(x,a,b):
-    return np.array([(1+b)**x,(a*x*(1+b)**x)/(1+b)]).T
-
+def model(t,a,b,c):
+    return c / (np.exp((b - t)/a) + 1)
+    
+def jacobian(t,a,b,c):
+    return np.array([c*(b - t)*np.exp((b - t)/a)/(a**2*(np.exp((b - t)/a) + 1)**2),
+                     -c*np.exp((b - t)/a)/(a*(np.exp((b - t)/a) + 1)**2),
+                     1/(np.exp((b - t)/a) + 1)]).T
 
 start_n = 10
 
@@ -31,14 +32,14 @@ x = casedata.x
 y = casedata.y
 
 # Do an initial fit to the whole data set to get the bounds.
-popt,pcov = curve_fit(model, x, y, p0=[1,1],sigma=None, method="lm",
+popt,pcov = curve_fit(model, x, y, p0=[1,1,1],sigma=None, method="lm",
                       jac=jacobian)
 perr = np.sqrt(np.diag(pcov))
 coef = describe(pcov)
 
 bhat = np.linspace(0, casedata.today-casedata.start+7, 100)
 
-upper = model(bhat, popt[0]+perr[0], popt[1]+perr[1])
+upper = model(bhat, popt[0]+perr[0], popt[1]+perr[1], popt[2]+perr[2])
 
 global_xlim = [0, bhat[-1]]
 global_ylim = [0, upper[-1]]
@@ -57,26 +58,29 @@ for panel in range(start_n,len(x)):
 
     # Levenburg-Marquardt Least-Squares Fit
     
-    popt,pcov = curve_fit(model, x[:panel], y[:panel], p0=[1,1],
+    popt,pcov = curve_fit(model, x[:panel], y[:panel], p0=[1,1,1],
                           sigma=None, method="lm", jac=jacobian)
 
     perr = np.sqrt(np.diag(pcov))
     coef = describe(pcov)
 
-    a, b = popt
+    a, b, c = popt
 
     # Use casedata, not the trailing item in the list, which for us
     # is not the largest abscissa!
     xhat = np.linspace(0, casedata.today-casedata.start+7, 100)
-    yhat = model(xhat,a,b)
+    yhat = model(xhat,a,b,c)
 
     upr_a = a+perr[0]
     upr_b = b+perr[1]
+    upr_c = c+perr[2]
+    
     lwr_a = a-perr[0]
     lwr_b = b-perr[1]
-
-    upper = model(xhat, upr_a, upr_b)
-    lower = model(xhat, lwr_a, lwr_b)
+    lwr_c = c-perr[2]
+    
+    upper = model(xhat, upr_a, upr_b, upr_c)
+    lower = model(xhat, lwr_a, lwr_b, lwr_c)
 
     ix = np.argsort(xhat)
     plt.plot(xhat[ix], yhat[ix], c="red", lw=1, zorder=5)
@@ -99,8 +103,9 @@ for panel in range(start_n,len(x)):
     else:
         fig_path = "."
 
-        plt.savefig("{0}/us_dc_panel_{1:02d}.png".format(fig_path,panel),
+        plt.savefig("{0}/us_dc_logpanel_{1:02d}.png".format(fig_path,panel),
                     dpi=150, bbox_inches="tight")
     plt.close()
 
 # Then you can maybe generate an animated gif or something.
+# convert -delay 20 -loop 0 <pattern> <name>.gif
