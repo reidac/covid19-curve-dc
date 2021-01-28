f3923d7d79f16c2601819b43dc5d3256b9daddf9
Andrew Reid <andrew.ce.reid@gmail.com>
2021-01-27 20:01:50 -0500

    Updated the code that generates the increment bar graph.

--- bars.py
+++ bars.py
@@ -20,8 +20,8 @@ total_incs = [casedata.positive[i+1]-casedata.positive[i] for i in range(inclen)
 pos_incs = [casedata.deaths[i+1]-casedata.deaths[i] for i in range(inclen)]
 # recov_incs = [casedata.recovered[i+1]-casedata.recovered[i] for i in range(inclen)]
 
-plt.bar(casedata.x[:-1],total_incs,color='b',width=0.4)
-plt.bar(casedata.x[:-1],pos_incs,color='r',width=0.4)
+plt.bar(casedata.x[:-1],total_incs,color='b',width=1.0)
+plt.bar(casedata.x[:-1],pos_incs,color='r',width=1.0)
 # plt.bar(casedata.x[:-1]+0.4,recov_incs,color='g',width=0.4)
 
 plt.legend(labels=['Positives','Deaths'])
